0913a1cde31fb7ab5884dd621200a2e29f95e65d
_shis <563000948@qq.com>
2016-12-06 02:47:05 +0800

    测试提交

--- SHIS/src/com/shis/test/Test.java
+++ SHIS/src/com/shis/test/Test.java
@@ -11,7 +11,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello word");
+		System.out.println("hello word123");
 	}
 
 }
