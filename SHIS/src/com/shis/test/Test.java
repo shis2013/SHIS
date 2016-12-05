f42b03b489c8844bb620cf0d5ef10385107304c3
_shis <563000948@qq.com>
2016-12-06 02:44:44 +0800

    提交测试类

---
+++ SHIS/src/com/shis/test/Test.java
@@ -0,0 +1,17 @@
+package com.shis.test;
+
+public class Test {
+
+	/** 
+	 * 创建人：史帅 
+	 * 创建时间：2016-12-6 上午02:43:50   
+	 * 方法描述：
+	 * @return void
+	 * @throws 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word");
+	}
+
+}
